f88e3db183d6310fe040101cfb9c9879ab8f9ca2
Prasad-Saravanan <prasadsaravanan11@gmail.com>
2021-11-23 14:16:41 +0800

    Project Bring-up

---
+++ Assets/Vuforia/Scripts/Vbuttonmanager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityEngine.Video;
+
+
+public class Vbuttonmanager : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public VideoPlayer player;
+    public GameObject playButton;
+
+    private AudioSource audioSource;
+    private bool paused1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        audioSource = GetComponent<AudioSource>();
+        paused1 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        
+        player.Play();
+        playButton.GetComponent<Renderer>().enabled = false;
+        paused1 = true;
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        player.Pause();
+        playButton.GetComponent<Renderer>().enabled = true;
+        paused1 = false;
+    }
+}
---
+++ Assets/Vuforia/Scripts/VideoPlaneController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlaneController : MonoBehaviour
+{
+    public VideoPlayer vp;
+
+    private void OnEnable()
+    {
+        if (vp !=null)vp.Play();
+    }
+
+    private void OnDisable()
+    {
+        if (vp != null) vp.Stop();
+    }
+}
